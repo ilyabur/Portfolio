bb568def83b0b7ab93304d580878733de838bfc2
Илья <ilya.cpp@gmail.com>
2022-07-18 00:07:38 +0300

    Adding JQuery script

--- JS/scripts.js
+++ JS/scripts.js
@@ -1,1 +1,21 @@
-console.log("Привет, скрипт подклучен!")
+console.log("Привет, скрипт подключен!")
+
+$(function() {
+ let header = $('.header_menu');
+ let hederHeight = header.height(); // вычисляем высоту шапки
+
+
+ $(window).scroll(function() {
+   if($(this).scrollTop() > 1) {
+    header.addClass('header_fixed');
+    $('.header_menu').css({
+        background": "white"
+    });
+   } else {
+    header.removeClass('header_fixed');
+    $('.header_menu').css({
+     'paddingTop': 0 // удаляю отступ у body, равный высоте шапки
+    })
+   }
+ });
+});
